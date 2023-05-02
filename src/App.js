867e310ff99046c16d09082718f222c2968856c7
hamzaoda <hamzaodawk@gmail.com>
2023-05-02 18:40:03 +0400

    changes the react route to homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Pages from "./pages/Pages";
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        <Router>
-        <Navbar/>
+        <React.Fragment>
+            <Navbar />
             <div className="App">
                 <Pages />
             </div>
-        <Footer/>
-        </Router>
+            <Footer />
+        </React.Fragment>
     );
 }
 
